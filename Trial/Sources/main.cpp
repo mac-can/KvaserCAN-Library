28d713d941a7d3a6257b84b1a98bfb6cddbe7ee1
Uwe McVogt <mac@uv-software.de>
2021-06-03 21:42:04 +0200

    Update the trial program
    Relates to [MACCAN-69]

--- Trial/Sources/main.cpp
+++ Trial/Sources/main.cpp
@@ -8,10 +8,14 @@
 
 #include <stdio.h>
 #include <string.h>
-#include <unistd.h>
 #include <signal.h>
 #include <errno.h>
 #include <time.h>
+#if !defined(_WIN32) && !defined(_WIN64)
+ #include <unistd.h>
+#else
+ #include <windows.h>
+#endif
 
 #include <inttypes.h>
 #include "MacCAN_Debug.h"
@@ -19,13 +23,27 @@
 //#define SECOND_CHANNEL
 #define ISSUE_198   (0)
 
+#define BITRATE_DEFAULT(x)  do{ x.btr.frequency=80000000; x.btr.nominal.brp=8; x.btr.nominal.tseg1= 31; x.btr.nominal.tseg2= 8; x.btr.nominal.sjw= 8; x.btr.data.brp=2; x.btr.data.tseg1=15; x.btr.data.tseg2=4; x.btr.data.sjw=2; } while(0)
+#define BITRATE_125K1M(x)   do{ x.btr.frequency=80000000; x.btr.nominal.brp=4; x.btr.nominal.tseg1=127; x.btr.nominal.tseg2=32; x.btr.nominal.sjw= 8; x.btr.data.brp=2; x.btr.data.tseg1=31; x.btr.data.tseg2=8; x.btr.data.sjw=2; } while(0)
+#define BITRATE_250K2M(x)   do{ x.btr.frequency=80000000; x.btr.nominal.brp=2; x.btr.nominal.tseg1=127; x.btr.nominal.tseg2=32; x.btr.nominal.sjw= 8; x.btr.data.brp=2; x.btr.data.tseg1=15; x.btr.data.tseg2=4; x.btr.data.sjw=2; } while(0)
+#define BITRATE_500K4M(x)   do{ x.btr.frequency=80000000; x.btr.nominal.brp=2; x.btr.nominal.tseg1= 63; x.btr.nominal.tseg2=16; x.btr.nominal.sjw= 8; x.btr.data.brp=2; x.btr.data.tseg1= 7; x.btr.data.tseg2=2; x.btr.data.sjw=2; } while(0)
+#define BITRATE_1M8M(x)     do{ x.btr.frequency=80000000; x.btr.nominal.brp=2; x.btr.nominal.tseg1= 31; x.btr.nominal.tseg2= 8; x.btr.nominal.sjw= 8; x.btr.data.brp=2; x.btr.data.tseg1= 3; x.btr.data.tseg2=1; x.btr.data.sjw=1; } while(0)
+#define BITRATE_125K(x)     do{ x.btr.frequency=80000000; x.btr.nominal.brp=4; x.btr.nominal.tseg1=127; x.btr.nominal.tseg2=32; x.btr.nominal.sjw= 8; } while(0)
+#define BITRATE_250K(x)     do{ x.btr.frequency=80000000; x.btr.nominal.brp=2; x.btr.nominal.tseg1=127; x.btr.nominal.tseg2=32; x.btr.nominal.sjw= 8; } while(0)
+#define BITRATE_500K(x)     do{ x.btr.frequency=80000000; x.btr.nominal.brp=2; x.btr.nominal.tseg1= 63; x.btr.nominal.tseg2=16; x.btr.nominal.sjw= 8; } while(0)
+#define BITRATE_1M(x)       do{ x.btr.frequency=80000000; x.btr.nominal.brp=2; x.btr.nominal.tseg1= 31; x.btr.nominal.tseg2= 8; x.btr.nominal.sjw= 8; } while(0)
+
 #define OPTION_NO   (0)
 #define OPTION_YES  (1)
+
 #define OPTION_TIME_DRIVER  (0)
 #define OPTION_TIME_ZERO    (1)
 #define OPTION_TIME_ABS     (2)
 #define OPTION_TIME_REL     (3)
 
+#if defined(_WIN32) || defined(_WIN64)
+ static void usleep(unsigned int usec);
+#endif
 static void sigterm(int signo);
 
 static void verbose(const can_mode_t mode, const can_bitrate_t bitrate, const can_speed_t speed);
@@ -60,10 +78,9 @@ int main(int argc, const char * argv[]) {
     message.timestamp.tv_sec = 0;
     message.timestamp.tv_nsec = 0;
     MacCAN_Return_t retVal = 0;
-    int32_t channel = 0;
+    int32_t channel = (int32_t)KVASER_CAN_CHANNEL0;
     uint16_t timeout = CANREAD_INFINITE;
     useconds_t delay = 0U;
-    time_t now = time(NULL);
     CMacCAN::EChannelState state;
     char szVal[CANPROP_MAX_BUFFER_SIZE];
     uint16_t u16Val;
@@ -83,17 +100,18 @@ int main(int argc, const char * argv[]) {
     int option_transmit = OPTION_NO;
     uint64_t received = 0ULL;
     uint64_t expected = 0ULL;
+    time_t now = time(NULL);
 
     for (int i = 1, opt = 0; i < argc; i++) {
         /* Kvaser CAN channel */
-        if(!strcmp(argv[i], "Kvaser CAN Channel 0") || !strcmp(argv[i], "CH:0")) channel = KVASER_CAN_CHANNEL0;
-        if(!strcmp(argv[i], "Kvaser CAN Channel 1") || !strcmp(argv[i], "CH:1")) channel = KVASER_CAN_CHANNEL1;
-        if(!strcmp(argv[i], "Kvaser CAN Channel 2") || !strcmp(argv[i], "CH:2")) channel = KVASER_CAN_CHANNEL2;
-        if(!strcmp(argv[i], "Kvaser CAN Channel 3") || !strcmp(argv[i], "CH:3")) channel = KVASER_CAN_CHANNEL3;
-        if(!strcmp(argv[i], "Kvaser CAN Channel 4") || !strcmp(argv[i], "CH:4")) channel = KVASER_CAN_CHANNEL4;
-        if(!strcmp(argv[i], "Kvaser CAN Channel 5") || !strcmp(argv[i], "CH:5")) channel = KVASER_CAN_CHANNEL5;
-        if(!strcmp(argv[i], "Kvaser CAN Channel 6") || !strcmp(argv[i], "CH:6")) channel = KVASER_CAN_CHANNEL6;
-        if(!strcmp(argv[i], "Kvaser CAN Channel 7") || !strcmp(argv[i], "CH:7")) channel = KVASER_CAN_CHANNEL7;
+        if (!strcmp(argv[i], "Kvaser CAN Channel 0") || !strcmp(argv[i], "CH:0")) channel = (int32_t)KVASER_CAN_CHANNEL0;
+        if (!strcmp(argv[i], "Kvaser CAN Channel 1") || !strcmp(argv[i], "CH:1")) channel = (int32_t)KVASER_CAN_CHANNEL1;
+        if (!strcmp(argv[i], "Kvaser CAN Channel 2") || !strcmp(argv[i], "CH:2")) channel = (int32_t)KVASER_CAN_CHANNEL2;
+        if (!strcmp(argv[i], "Kvaser CAN Channel 3") || !strcmp(argv[i], "CH:3")) channel = (int32_t)KVASER_CAN_CHANNEL3;
+        if (!strcmp(argv[i], "Kvaser CAN Channel 4") || !strcmp(argv[i], "CH:4")) channel = (int32_t)KVASER_CAN_CHANNEL4;
+        if (!strcmp(argv[i], "Kvaser CAN Channel 5") || !strcmp(argv[i], "CH:5")) channel = (int32_t)KVASER_CAN_CHANNEL5;
+        if (!strcmp(argv[i], "Kvaser CAN Channel 6") || !strcmp(argv[i], "CH:6")) channel = (int32_t)KVASER_CAN_CHANNEL6;
+        if (!strcmp(argv[i], "Kvaser CAN Channel 7") || !strcmp(argv[i], "CH:7")) channel = (int32_t)KVASER_CAN_CHANNEL7;
         /* baud rate (CAN 2.0) */
         if (!strcmp(argv[i], "BD:0") || !strcmp(argv[i], "BD:1000")) bitrate.index = CANBTR_INDEX_1M;
         if (!strcmp(argv[i], "BD:1") || !strcmp(argv[i], "BD:800")) bitrate.index = CANBTR_INDEX_800K;
@@ -104,6 +122,19 @@ int main(int argc, const char * argv[]) {
         if (!strcmp(argv[i], "BD:6") || !strcmp(argv[i], "BD:50")) bitrate.index = CANBTR_INDEX_50K;
         if (!strcmp(argv[i], "BD:7") || !strcmp(argv[i], "BD:20")) bitrate.index = CANBTR_INDEX_20K;
         if (!strcmp(argv[i], "BD:8") || !strcmp(argv[i], "BD:10")) bitrate.index = CANBTR_INDEX_10K;
+        /* CAN FD operation */
+        if(!strcmp(argv[i], "CANFD") || !strcmp(argv[i], "FD")) opMode.fdoe = 1;
+        if(!strcmp(argv[i], "FDF")) opMode.fdoe = 1;
+        if(!strcmp(argv[i], "BRS")) opMode.brse = 1;
+        /* bit rate (CAN FD) */
+        if(!strcmp(argv[i], "BR:125K1M")) BITRATE_125K1M(bitrate);
+        if(!strcmp(argv[i], "BR:250K2M")) BITRATE_250K2M(bitrate);
+        if(!strcmp(argv[i], "BR:500K4M")) BITRATE_500K4M(bitrate);
+        if(!strcmp(argv[i], "BR:1M8M")) BITRATE_1M8M(bitrate);
+        if(!strcmp(argv[i], "BR:125K")) BITRATE_125K(bitrate);
+        if(!strcmp(argv[i], "BR:250K")) BITRATE_250K(bitrate);
+        if(!strcmp(argv[i], "BR:500K")) BITRATE_500K(bitrate);
+        if(!strcmp(argv[i], "BR:1M")) BITRATE_1M(bitrate);
         /* asynchronous IO */
         if (!strcmp(argv[i], "POLLING")) timeout = 0U;
         if (!strcmp(argv[i], "BLOCKING")) timeout = CANREAD_INFINITE;
@@ -247,11 +278,7 @@ int main(int argc, const char * argv[]) {
             fprintf(stdout, ">>> myDriver.GetProperty(KVASERCAN_PROPERTY_DEVICE_VENDOR): value = '%s'\n", szVal);
         else
             fprintf(stderr, "+++ error: myDriver.GetProperty(KVASERCAN_PROPERTY_DEVICE_VENDOR) returned %i\n", retVal);
-//        retVal = myDriver.GetProperty(KVASERCAN_PROPERTY_VENDOR_URL, (void *)szVal, CANPROP_MAX_BUFFER_SIZE);
-//        if (retVal == CMacCAN::NoError)
-//            fprintf(stdout, ">>> myDriver.GetProperty(KVASERCAN_PROPERTY_VENDOR_URL): value = '%s'\n", szVal);
-//        else
-//            fprintf(stderr, "+++ error: myDriver.GetProperty(KVASERCAN_PROPERTY_VENDOR_URL) returned %i\n", retVal);
+        // vendor-specific properties
 //        retVal = myDriver.GetProperty(KVASERCAN_PROPERTY_CLOCK_DOMAIN, (void *)&i32Val, sizeof(int32_t));
 //        if (retVal == CMacCAN::NoError)
 //            fprintf(stdout, ">>> myDriver.GetProperty(KVASERCAN_PROPERTY_CLOCK_DOMAIN): value = %d\n", i32Val);
@@ -263,7 +290,9 @@ int main(int argc, const char * argv[]) {
         else
             fprintf(stderr, "+++ error: myDriver.GetProperty(KVASERCAN_PROPERTY_OP_CAPABILITY) returned %i\n", retVal);
         if (myDriver.GetProperty(KVASERCAN_PROPERTY_OP_MODE, (void *)&opMode.byte, sizeof(uint8_t)) == CMacCAN::NoError)
-            fprintf(stdout, ">>> myDriver.Op.-Mode: 0x%02X\n", (uint8_t)opMode.byte);
+            fprintf(stdout, ">>> myDriver.GetProperty(KVASERCAN_PROPERTY_OP_MODE): value = 0x%02X\n", (uint8_t)opMode.byte);
+        if (myDriver.GetProperty(KVASERCAN_PROPERTY_STATUS, (void *)&status.byte, sizeof(uint8_t)) == CMacCAN::NoError)
+            fprintf(stdout, ">>> myDriver.GetProperty(KVASERCAN_PROPERTY_STATUS): value = 0x%02X\n", (uint8_t)status.byte);
     }
     retVal = myDriver.StartController(bitrate);
     if (retVal != CMacCAN::NoError) {
@@ -290,8 +319,20 @@ int main(int argc, const char * argv[]) {
     if (retVal != CMacCAN::NoError)
         fprintf(stderr, "+++ error: mySecond.WriteMessage returned %i\n", retVal);
 #endif
+    if ((option_transmit > 0) && !option_retry)
+        fprintf(stdout, "Attention: The program will be aborted when the transmitter is busy.\n"
+                        "           Use progrsm option RETRY to avoid this.\n");
     fprintf(stdout, "Press Ctrl+C to abort...\n");
     while (running && (option_transmit-- > 0)) {
+        if (!opMode.fdoe) {
+            message.fdf = 0;
+            message.brs = 0;
+            message.dlc = CAN_MAX_DLC;
+        } else {
+            message.fdf = opMode.fdoe;
+            message.brs = opMode.brse;
+            message.dlc = CANFD_MAX_DLC;
+        }
 retry:
         retVal = myDriver.WriteMessage(message);
         if ((retVal == CMacCAN::TransmitterBusy) && option_retry)
@@ -306,11 +347,14 @@ retry:
     while (running) {
         if ((retVal = myDriver.ReadMessage(message, timeout)) == CMacCAN::NoError) {
             if (option_echo) {
-                fprintf(stdout, ">>> %-6i  ", frames++);
-                fprintf(stdout, "%7li.%04li  %03x %c%c [%i]",
-                                 message.timestamp.tv_sec, message.timestamp.tv_nsec / 100000,
-                                 message.id, message.xtd? 'X' : 'S', message.rtr? 'R' : ' ', message.dlc);
-                for (int i = 0; i < message.dlc; i++)
+                fprintf(stdout, ">>> %i\t", frames++);
+                fprintf(stdout, "%7li.%04li\t", (long)message.timestamp.tv_sec, message.timestamp.tv_nsec / 100000);
+                if (!opMode.fdoe)
+                    fprintf(stdout, "%03x\t%c%c [%i]", message.id, message.xtd ? 'X' : 'S', message.rtr ? 'R' : ' ', message.dlc);
+                else
+                    fprintf(stdout, "%03x\t%c%c%c%c%c [%i]", message.id, message.xtd ? 'X' : 'S', message.rtr ? 'R' : ' ',
+                            message.fdf ? 'F' : ' ', message.brs ? 'B' : ' ', message.esi ? 'E' :' ', CMacCAN::Dlc2Len(message.dlc));
+                for (uint8_t i = 0; i < CMacCAN::Dlc2Len(message.dlc); i++)
                     fprintf(stdout, " %02x", message.data[i]);
                 if (message.sts)
                     fprintf(stdout, " <<< status frame");
@@ -354,11 +398,14 @@ retry:
 #ifdef SECOND_CHANNEL
         if ((retVal = mySecond.ReadMessage(message, 0U)) == CMacCAN::NoError) {
             if (option_echo) {
-                fprintf(stdout, ">2> %-6i  ", frames++);
-                fprintf(stdout, "%7li.%04li  %03x %c%c [%i]",
-                                 message.timestamp.tv_sec, message.timestamp.tv_nsec / 100000,
-                                 message.id, message.xtd? 'X' : 'S', message.rtr? 'R' : ' ', message.dlc);
-                for (int i = 0; i < message.dlc; i++)
+                fprintf(stdout, ">2> %i\t", frames++);
+                fprintf(stdout, "%7li.%04li\t", (long)message.timestamp.tv_sec, message.timestamp.tv_nsec / 100000);
+                if (!opMode.fdoe)
+                    fprintf(stdout, "%03x\t%c%c [%i]", message.id, message.xtd ? 'X' : 'S', message.rtr ? 'R' : ' ', message.dlc);
+                else
+                    fprintf(stdout, "%03x\t%c%c%c%c%c [%i]", message.id, message.xtd ? 'X' : 'S', message.rtr ? 'R' : ' ',
+                        message.fdf ? 'F' : ' ', message.brs ? 'B' : ' ', message.esi ? 'E' : ' ', CMacCAN::Dlc2Len(message.dlc));
+                for (uint8_t i = 0; i < CMacCAN::Dlc2Len(message.dlc); i++)
                     fprintf(stdout, " %02x", message.data[i]);
                 if (message.sts)
                     fprintf(stdout, " <<< status frame");
@@ -437,55 +484,45 @@ end:
 
 static void verbose(const can_mode_t mode, const can_bitrate_t bitrate, const can_speed_t speed)
 {
-#if (OPTION_CAN_2_0_ONLY == 0)
     fprintf(stdout, "Op.-Mode: 0x%02X (fdoe=%u,brse=%u,niso=%u,shrd=%u,nxtd=%u,nrtr=%u,err=%u,mon=%u)\n",
             mode.byte, mode.fdoe, mode.brse, mode.niso, mode.shrd, mode.nxtd, mode.nrtr, mode.err, mode.mon);
-#else
-    fprintf(stdout, "Op.-Mode: 0x%02X (shrd=%u,nxtd=%u,nrtr=%u,err=%u,mon=%u)\n",
-            mode.byte, mode.shrd, mode.nxtd, mode.nrtr, mode.err, mode.mon);
-#endif
-   if (bitrate.btr.frequency > 0) {
+    if(bitrate.btr.frequency > 0) {
         fprintf(stdout, "Baudrate: %.0fkbps@%.1f%%",
-           speed.nominal.speed / 1000., speed.nominal.samplepoint * 100.);
-#if (OPTION_CAN_2_0_ONLY == 0)
+            speed.nominal.speed / 1000., speed.nominal.samplepoint * 100.);
         if(/*speed.data.brse*/mode.fdoe && mode.brse)
-           fprintf(stdout, ":%.0fkbps@%.1f%%",
-               speed.data.speed / 1000., speed.data.samplepoint * 100.);
-#endif
-       fprintf(stdout, " (f_clock=%i,nom_brp=%u,nom_tseg1=%u,nom_tseg2=%u,nom_sjw=%u,nom_sam=%u",
-           bitrate.btr.frequency,
-           bitrate.btr.nominal.brp,
-           bitrate.btr.nominal.tseg1,
-           bitrate.btr.nominal.tseg2,
-           bitrate.btr.nominal.sjw,
-           bitrate.btr.nominal.sam);
-#if (OPTION_CAN_2_0_ONLY == 0)
+            fprintf(stdout, ":%.0fkbps@%.1f%%",
+                speed.data.speed / 1000., speed.data.samplepoint * 100.);
+        fprintf(stdout, " (f_clock=%i,nom_brp=%u,nom_tseg1=%u,nom_tseg2=%u,nom_sjw=%u,nom_sam=%u",
+            bitrate.btr.frequency,
+            bitrate.btr.nominal.brp,
+            bitrate.btr.nominal.tseg1,
+            bitrate.btr.nominal.tseg2,
+            bitrate.btr.nominal.sjw,
+            bitrate.btr.nominal.sam);
         if(mode.fdoe && mode.brse)
-           fprintf(stdout, ",data_brp=%u,data_tseg1=%u,data_tseg2=%u,data_sjw=%u",
-               bitrate.btr.data.brp,
-               bitrate.btr.data.tseg1,
-               bitrate.btr.data.tseg2,
-               bitrate.btr.data.sjw);
-#endif
-       fprintf(stdout, ")\n");
+            fprintf(stdout, ",data_brp=%u,data_tseg1=%u,data_tseg2=%u,data_sjw=%u",
+                bitrate.btr.data.brp,
+                bitrate.btr.data.tseg1,
+                bitrate.btr.data.tseg2,
+                bitrate.btr.data.sjw);
+        fprintf(stdout, ")\n");
     }
     else {
         fprintf(stdout, "Baudrate: %skbps (CiA index %i)\n",
-           bitrate.index == CANBDR_1000 ? "1000" :
-           bitrate.index == -CANBDR_800 ? "800" :
-           bitrate.index == -CANBDR_500 ? "500" :
-           bitrate.index == -CANBDR_250 ? "250" :
-           bitrate.index == -CANBDR_125 ? "125" :
-           bitrate.index == -CANBDR_100 ? "100" :
-           bitrate.index == -CANBDR_50 ? "50" :
-           bitrate.index == -CANBDR_20 ? "20" :
-           bitrate.index == -CANBDR_10 ? "10" : "?", -bitrate.index);
-   }
+            bitrate.index == CANBDR_1000 ? "1000" :
+            bitrate.index == -CANBDR_800 ? "800" :
+            bitrate.index == -CANBDR_500 ? "500" :
+            bitrate.index == -CANBDR_250 ? "250" :
+            bitrate.index == -CANBDR_125 ? "125" :
+            bitrate.index == -CANBDR_100 ? "100" :
+            bitrate.index == -CANBDR_50 ? "50" :
+            bitrate.index == -CANBDR_20 ? "20" :
+            bitrate.index == -CANBDR_10 ? "10" : "?", -bitrate.index);
+    }
 }
 
 static void sigterm(int signo) {
     //fprintf(stderr, "%s: got signal %d\n", __FILE__, signo);
-    // FIXME: leads into a deadlock
     (void)myDriver.SignalChannel();
 #ifdef SECOND_CHANNEL
     (void)mySecond.SignalChannel();
