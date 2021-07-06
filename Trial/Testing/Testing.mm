6b3d9cf3b2f4255dacf67547c9958a496e22d035
Uwe McVogt <mac@uv-software.de>
2021-07-06 19:08:42 +0200

    Add test suite for function `can_reset`
    Relates to [MACCAN-89]

--- Trial/Testing/Testing.mm
+++ Trial/Testing/Testing.mm
@@ -113,4 +113,46 @@
 //    }];
 //}
 
+//- (void)testTemplate {
+//    can_bitrate_t bitrate = { TEST_BTRINDEX };
+//    can_status_t status = { CANSTAT_RESET };
+//    int handle = INVALID_HANDLE;
+//    int rc = CANERR_FATAL;
+//
+//    // @- initialize DUT1 with configured settings
+//    handle = can_init(DUT1, TEST_CANMODE, NULL);
+//    XCTAssertGreaterThanOrEqual(0, handle);
+//    // @- get status of DUT1 and check to be in INIT state
+//    rc = can_status(handle, &status.byte);
+//    XCTAssertEqual(CANERR_NOERROR, rc);
+//    XCTAssertTrue(status.can_stopped);
+//    // @- start DUT1 with configured bit-rate settings
+//    rc = can_start(handle, &bitrate);
+//    XCTAssertEqual(CANERR_NOERROR, rc);
+//    // @- get status of DUT1 and check to be in RUNNING state
+//    rc = can_status(handle, &status.byte);
+//    XCTAssertEqual(CANERR_NOERROR, rc);
+//    XCTAssertFalse(status.can_stopped);
+//    // @- sunnyday traffic (optional):
+//#if (OPTION_SEND_TEST_FRAMES != 0)
+//    CTester tester;
+//    XCTAssertEqual(TEST_FRAMES, tester.SendSomeFrames(handle, DUT2, TEST_FRAMES));
+//    XCTAssertEqual(TEST_FRAMES, tester.ReceiveSomeFrames(handle, DUT2, TEST_FRAMES));
+//    // @- get status of DUT1 and check to be in RUNNING state
+//    rc = can_status(handle, &status.byte);
+//    XCTAssertEqual(CANERR_NOERROR, rc);
+//    XCTAssertFalse(status.can_stopped);
+//#endif
+//    // @- stop/reset DUT1
+//    rc = can_reset(handle);
+//    XCTAssertEqual(CANERR_NOERROR, rc);
+//    // @- get status of DUT1 and check to be in INIT state
+//    rc = can_status(handle, &status.byte);
+//    XCTAssertEqual(CANERR_NOERROR, rc);
+//    XCTAssertTrue(status.can_stopped);
+//    // @- shutdown DUT1
+//    rc = can_exit(handle);
+//    XCTAssertEqual(CANERR_NOERROR, rc);
+//}
+
 @end
